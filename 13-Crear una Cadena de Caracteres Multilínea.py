660addbdc18ea4f38efd2a737f5b57ed158dfc23
Juan <kaenel10@gmail.com>
2020-07-18 17:42:22 -0300

    Ejercicio 13

---
+++ 13-Crear una Cadena de Caracteres Multilínea.py
@@ -0,0 +1,17 @@
+#Crear una cadena de caracteres multilinea
+
+#forma ineficiente y complicada
+cadena = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. "
+cadena +="Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+
+#en la forma eficiente utilizamos docstrings
+cadena1 = """
+Lorem Ipsum is simply dummy text of the printing and typesetting
+industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+It has survived not only five centuries, but also the leap into electronic typesetting, remaining
+essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+"""
+
+print(cadena1)
