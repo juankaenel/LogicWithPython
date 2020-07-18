55b2ec0ed141839ed3a1660088e466d4b86aa732
Juan <kaenel10@gmail.com>
2020-07-18 17:31:29 -0300

    Ejercicio 11

---
+++ 11- Obtener la Documentación de Funciones Incorporadas.py
@@ -0,0 +1,14 @@
+#Obtener la documentación de funciones incorporadas
+from datetime import datetime
+
+print('-'*70)
+
+print(int.__doc__) #con __doc__ accedo a la documetación de las funciones
+
+print('-'*70)
+
+print(datetime.__doc__)
+
+print('-'*70)
+
+print(str.split.__doc__)
