1468d0194a90bb3eb79c4d97dd69fc4ec9c416a2
Juan <kaenel10@gmail.com>
2020-08-07 02:27:30 -0300

    Ejercicio 26

---
+++ 26 - Emular funcionamiento de la función join.py
@@ -0,0 +1,19 @@
+#emular el funcionamiento de la funcion join para concatenar una lista
+numeros = [2,3,4,7,10]
+# 234710
+#con list comprehension
+#list = [ expresion for valor in iterable if condicion] siendo la condicion opcional
+
+#print(''.join([str(n) for n in numeros])), me va a devolver un join por cada n in numeros
+
+#sin list comprehension
+#for n in numeros:
+#    print (''.join(str(n)),end='')
+
+def concatenar_lista(lista):
+    resultado = ''
+    for n in lista:
+        resultado += str(n)
+    return resultado
+
+print(concatenar_lista(numeros))
